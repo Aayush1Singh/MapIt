91df5c5e4a399ef172cae290c000be58550fde4a
aayush <aayush>
2025-03-13 17:58:18 +0530

    d

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,7 +1,7 @@
 // npm run de
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
-import Map from "../components/map";
+import Map from "../components/Map";
 function AppLayout() {
   return (
     <div className={styles.app}>
